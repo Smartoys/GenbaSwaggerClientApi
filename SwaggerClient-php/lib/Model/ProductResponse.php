f9d71612c0100e4209fccac4606eb2d9c0da2b94
HorstmanCLD <t.gueu@cld.be>
2024-05-16 15:14:22 +0200

    fix(general); update return

--- SwaggerClient-php/lib/Model/ProductResponse.php
+++ SwaggerClient-php/lib/Model/ProductResponse.php
@@ -841,9 +841,9 @@ class ProductResponse implements ModelInterface, ArrayAccess
      *
      * @param integer $offset Offset
      *
-     * @return boolean
+     * @return bool
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -855,9 +855,9 @@ class ProductResponse implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
-        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+        return $this->container[$offset] ?? null;
     }
 
     /**
@@ -868,7 +868,7 @@ class ProductResponse implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -884,7 +884,7 @@ class ProductResponse implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
